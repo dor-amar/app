e7f156028e7c462f4479e25710f0389ae209f6ed
Dor Amar <doramar5555@gmail.com>
2025-09-29 20:13:28 +0300

    Change

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return jsonify(message="Hello from Flask in Docker!")
+    return jsonify(message="Full CICD!")
 
 @app.route("/echo", methods=["POST"])
 def echo():
